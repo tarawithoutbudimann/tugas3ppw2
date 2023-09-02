71920f388a24b1ac802e224dabdea2be9b26b57d
Tamara Sashikirana <113746952+tarawithoutbudimann@users.noreply.github.com>
2023-09-03 06:37:11 +0700

    Add files via upload

---
+++ public/js/script.js
@@ -0,0 +1,14 @@
+document.getElementById("button").addEventListener('click', function(){
+    document.querySelector(".share").style.display = 'flex';
+    document.getElementById("buttonClose").style.display = 'inline-block';
+    document.getElementById("button").style.display = 'none';
+});
+
+function init(){
+    document.querySelector(".share").style.display = 'none';
+    document.getElementById("button").style.display = 'inline-block'; 
+    document.getElementById("buttonClose").style.display = 'none';
+};
+
+document.getElementById("buttonClose").addEventListener('click', init);
+document.getElementById("buttonCloseOne").addEventListener('click', init);
---
+++ storage/framework/views/7c482dece5a19a067e4a2aa4576ee358.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!-- displays site properly based on user's device -->
+
+  <link rel="icon" type="image/png" sizes="32x32" href="./images/favicon-32x32.png">
+  <link rel="stylesheet" href="">
+  <title>Frontend Mentor | Article preview component</title>
+  <link rel="stylesheet" href="css/style.css">
+  <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@700&display=swap" rel="stylesheet">
+</head>
+<body>
+  <div class="body">
+      <img id="mainImg" src="images/drawers.jpg" width="100%">
+    <div class="text">
+      <div class="heading">
+        Shift the overall look and feel by adding these wonderful 
+        touches to furniture in your home
+      </div>
+      <div class="subText">
+        Ever been in a room and felt like something was missing? Perhaps 
+        it felt slightly bare and uninviting. I’ve got some simple tips 
+        to help you make any room feel complete.
+      </div>
+
+      <div class="person">
+        <div class="images">
+          <img src="images/avatar-michelle.jpg" width="40px">
+          <div class="name">
+            <p>Michelle Appleton</p> 
+            <font color="#9EAFC2">28 Jun 2020</font>
+          </div>
+        </div>
+
+        <button id="button">
+          <img src="images/icon-share.svg">
+        </button>
+        <button id="buttonClose">
+          <img src="images/icon-share.svg">
+        </button>
+      </div>
+      <div class="share">
+        S H A R E
+        <div class="sharesvg">
+          <img src="images/icon-facebook.svg"> &ensp;
+          <img src="images/icon-twitter.svg"> &ensp;
+          <img src="images/icon-pinterest.svg">
+        </div>
+        <button id="buttonCloseOne">
+          <img src="images/icon-share.svg">
+        </button>
+        <div class ="triangle"></div>
+
+      </div>
+    </div>
+
+  </div>
+  <script src="js/script.js"></script>
+</body>
+</html><?php /**PATH C:\xampp\htdocs\blog\resources\views/about.blade.php ENDPATH**/ ?>
